ba3a20ad1f876f0d528e8b002e8198bdd277918e
Mümin Yıldırım <mmnyldrm61@gmail.com>
2020-06-20 17:10:19 +0300

    css

--- pages/_document.js
+++ pages/_document.js
@@ -91,10 +91,6 @@ class MyDocument extends Document {
             name="apple-mobile-web-app-title"
             content="Puuwto"
           />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800;900&display=swap"
-            rel="stylesheet"
-          />
         </Head>
         <body>
           <Main />
