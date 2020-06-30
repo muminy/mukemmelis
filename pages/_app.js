da6665c31799fca020743897f447fb6ac481a295
Mümin Yıldırım <mmnyldrm61@gmail.com>
2020-06-30 04:17:56 +0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,4 @@
 import '../styles/style.css';
-import '../styles/bootstrap.css';
 import '../styles/responsive.css';
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
