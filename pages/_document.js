df7e79b9004f2d539ad31e7e3ab1f761b059a4d6
Mümin Yıldırım <mmnyldrm61@gmail.com>
2020-06-30 03:42:24 +0300

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -31,6 +31,12 @@ class MyDocument extends Document {
             httpEquiv="x-ua-compatible"
             content="ie=edge"
           />
+          <link
+            rel="stylesheet"
+            href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/css/bootstrap.min.css"
+            integrity="sha384-r4NyP46KrjDleawBgD5tp8Y7UzmLA05oM1iAEQ17CSuDqnUK2+k9luXQOfXJCJ4I"
+            crossOrigin="anonymous"
+          ></link>
           <meta
             name="viewport"
             content="width=device-width, initial-scale=1"
